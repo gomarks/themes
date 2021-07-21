ce932c46ed6fe06b8b2e8a1d572bc4e5e1215aed
916anan <40131953+916anan@users.noreply.github.com>
2021-07-21 16:09:25 +0800

    [新增功能](master): 引入官方已经 combine 的 js

--- bjca/resource/assets_path.go
+++ bjca/resource/assets_path.go
@@ -2,12 +2,12 @@ package resource
 
 var AssetPaths = map[string]string{
 	"all.min.css":      "/dist/css/all.min.0c9b0dc4a0.css",
-	"all.min.js":       "/dist/js/all.min.ea3820d05e.js",
-	"all_2.min.js":     "/dist/js/all_2.min.f7e4017899.js",
-	"datatable.min.js": "/dist/js/datatable.min.587502240b.js",
-	"form.min.js":      "/dist/js/form.min.4fb28a4ab3.js",
+	"all.min.js":       "/dist/js/all.min.8425540791.js",
+	"all_2.min.js":     "/dist/js/all_2.min.38a2a946b0.js",
+	"datatable.min.js": "/dist/js/datatable.min.581cdc109b.js",
+	"form.min.js":      "/dist/js/form.min.f8678914e9.js",
 	"html5shiv.min.js": "/dist/js/html5shiv.min.js",
 	"respond.min.js":   "/dist/js/respond.min.js",
-	"tree.min.js":      "/dist/js/tree.min.5b78a890b8.js",
-	"treeview.min.js":  "/dist/js/treeview.min.1681c27992.js",
+	"tree.min.js":      "/dist/js/tree.min.e1faf8b7de.js",
+	"treeview.min.js":  "/dist/js/treeview.min.7780d3bb0f.js",
 }
